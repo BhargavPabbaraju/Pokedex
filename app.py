a81300a701562d68bea3cf19c16c367e8f4b4bf4
BHARGAV P <bhargav@BHARGAVs-MacBook-Air.local>
2023-06-22 17:05:48 +0530

    scraped pokemon data and pokedex working

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask,render_template,request
 import pymongo
 import certifi
 
+from data import typeImages
+from scrape_data import Scrapper
+
 app = Flask(__name__)
 
 connection = "mongodb+srv://user1:PassWord@cluster0.wpysgqb.mongodb.net/?retryWrites=true&w=majority"
@@ -10,16 +13,18 @@ db =client['poke']
 coll = db['pokemon_list']
 pokemon_list = sorted(set([ x['name'] for x in coll.find()]))
 
+scrapper = Scrapper()
+
 
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html",pokes=pokemon_list)
+    return render_template("index.html",pokes=pokemon_list,typeImages=typeImages,poke_data=None)
 
 @app.route("/search",methods=["POST"])
 def search():
     searchName = request.form["select-poke"]
-    return f"<h1>{searchName}</h1>"
+    return render_template("index.html",pokes=pokemon_list,typeImages=typeImages,poke_data = scrapper.scrape_data(searchName))
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5001,debug=True)
---
+++ scrape_data.py
@@ -0,0 +1,98 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.request import Request,urlopen
+
+
+class Scrapper:
+    def __init__(self):
+        self.poke_data={}
+        self.name = "Bulbasaur"
+    
+    def scrape_data(self,name):
+        url = f"https://pokemondb.net/pokedex/{name}"
+        site = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+
+        urlclient = urlopen(site).read()
+        self.html = bs(urlclient,"html.parser")
+        forms = self.html.find_all("div",{"class":"sv-tabs-tab-list"})[0].find_all("a")
+        form = 0
+        images = self.html.find_all("img",{"fetchpriority":"high"})[1:]
+        self.poke_data['image_link'] = images[form]['src'].strip('\n')
+        self.poke_data['name'] = name
+
+        self.dex()
+
+        self.training()
+
+        self.breeding()
+        self.base_stats()
+        self.dex_entry()
+
+        return self.poke_data
+
+
+    def dex(self):
+        #Pokédex data
+        table = self.html.find("h2",string="Pokédex data").find_next("table",{"class":"vitals-table"})
+
+        tds = table.find_all("td")
+        self.poke_data["index"] = '#'+tds[0].text
+        self.poke_data['types']=[]
+        for a in tds[1].find_all("a"):
+            self.poke_data['types'].append(a.text.strip('\n'))
+
+        self.poke_data['species'] = tds[2].text.strip('\n')
+        ht = tds[3].text.strip('\n')
+        self.poke_data['ht'] = ht[:ht.index('m')+1]
+        wt = tds[4].text.strip('\n')
+        self.poke_data['wt'] = wt[:wt.index('g')+1]
+        self.poke_data['abilities']=[]
+        for a in tds[5].find_all('a'):
+            name = a.text.strip('\n')
+            desc = a['title']
+            self.poke_data['abilities'].append({name:desc})
+
+
+    def training(self):
+        #Training
+        table = self.html.find("h2",string="Training").find_next("table",{"class":"vitals-table"})
+        tds = table.find_all("td")
+        self.poke_data['catch rate'] = tds[1].text.strip('\n')
+        self.poke_data['catch rate'] = self.poke_data['catch rate'][:self.poke_data['catch rate'].index('(')]
+        self.poke_data['happiness'] = tds[2].text.strip('\n')
+        self.poke_data['happiness'] = self.poke_data['happiness'][:self.poke_data['happiness'].index('(')]
+        self.poke_data['base exp'] = tds[3].text.strip('\n')
+        self.poke_data['growth rate'] = tds[4].text.strip('\n')
+
+    def breeding(self):
+        #Breeding
+        table = self.html.find("h2",string="Breeding").find_next("table",{"class":"vitals-table"})
+        tds = table.find_all("td")
+        self.poke_data['egg group'] = tds[0].text.strip('\n')
+        self.poke_data['gender ratio']=[]
+        for span in tds[1].find_all('span'):
+            ratio = span.text.strip('\n')
+            self.poke_data['gender ratio'].append(ratio[:ratio.find('%')])
+
+        self.poke_data['egg cycles'] = tds[2].text.strip('\n')
+        self.poke_data['egg cycles'] = self.poke_data['egg cycles'][:self.poke_data['egg cycles'].index('(')]
+
+    def base_stats(self):
+        #Base Stats
+        table = self.html.find("h2",string="Base stats").find_next("table",{"class":"vitals-table"})
+        tds = table.find_all("td")
+        self.poke_data['hp'] = tds[0].text.strip('\n')
+        self.poke_data['atk'] = tds[4].text.strip('\n')
+        self.poke_data['def'] = tds[8].text.strip('\n')
+        self.poke_data['sp.atk'] = tds[12].text.strip('\n')
+        self.poke_data['sp.def'] = tds[16].text.strip('\n')
+        self.poke_data['speed'] = tds[20].text.strip('\n')
+        self.poke_data['total'] = tds[24].text.strip('\n')
+    
+
+    def dex_entry(self):
+        #Dex Entries
+        table = self.html.find("h2",string="Pokédex entries").find_next("table",{"class":"vitals-table"})
+        tds = table.find_all('td')
+        self.poke_data['desc'] = tds[-1].text.strip('\n')
+    
