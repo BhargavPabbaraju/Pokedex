e9d33dadc36b75464de42762c8b42ebabf2cfc55
BHARGAV P <bhargav@BHARGAVs-MacBook-Air.local>
2023-06-23 19:24:59 +0530

    form changes work

--- app.py
+++ app.py
@@ -15,18 +15,42 @@ pokemon_list = sorted(set([ x['name'] for x in coll.find()]))
 
 scrapper = Scrapper()
 
+currentForm = 0
+poke = None
+currentPoke = None
 
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html",pokes=pokemon_list,typeImages=typeImages,poke_data=None)
+    return render_template("index.html",pokes=pokemon_list,typeImages=typeImages,poke_data=None,form=0)
 
 @app.route("/search",methods=["POST"])
 def search():
+    global currentForm,currentPoke,poke
     searchName = request.form["select-poke"]
-    form = 0
-    poke = coll.find({"name":searchName})[0]['data'][form]
-    return render_template("index.html",pokes=pokemon_list,typeImages=typeImages,poke_data = poke)
+    
+    if searchName!=currentPoke:
+        currentPoke = searchName
+        currentForm = 0
+
+    poke = coll.find({"name":searchName})[0]['data']
+    return render_template("index.html",pokes=pokemon_list,typeImages=typeImages,poke_data = poke,form=currentForm)
+
+
+@app.route("/formchange",methods=["POST"])
+def formchange():
+    global currentForm
+    typ = request.form['action']
+
+    if(typ=="next"):
+        currentForm+=1
+    else:
+        currentForm-=1
+    
+    
+    return render_template("index.html",pokes=pokemon_list,typeImages=typeImages,poke_data = poke,form=currentForm)
+    
+    
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5001,debug=True)
